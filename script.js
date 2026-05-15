a566b05c765874c0d7806dada124a4fbc547b7f7
Vifernel <zounmavifernel@gmail.com>
2026-05-15 05:53:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -141,3 +141,36 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+const menuToggle =
+document.querySelector(".menu-toggle");
+
+const navMenu =
+document.querySelector(".nav-menu");
+
+menuToggle.addEventListener(
+"click",
+() => {
+
+navMenu.classList.toggle("active");
+
+menuToggle.classList.toggle("active");
+
+});
+
+/* CLOSE MENU WHEN CLICK */
+
+document
+.querySelectorAll(".nav-menu a")
+.forEach(link => {
+
+link.addEventListener(
+"click",
+() => {
+
+navMenu.classList.remove("active");
+
+menuToggle.classList.remove("active");
+
+});
+
+});
